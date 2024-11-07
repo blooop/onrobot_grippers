2252d19aa8c4df56eeac88c739ac4dab2bccf5e5
Austin Gregg-Smith <blooop@gmail.com>
2024-11-07 14:43:05 +0000

    port python code to ros2

--- onrobot_vg_control/onrobot_vg_control/baseOnRobotVG.py
+++ onrobot_vg_control/onrobot_vg_control/baseOnRobotVG.py
@@ -1,80 +1,101 @@
 #!/usr/bin/env python3
 
-import rospy
+import rclpy
+from rclpy.node import Node
 from onrobot_vg_control.msg import OnRobotVGInput
 
 
-class onrobotbaseVG:
+class OnRobotBaseVG(Node):
     """Base class (communication protocol agnostic) for sending commands
        and receiving the status of the OnRobot VG gripper.
     """
 
     def __init__(self):
+        super().__init__('onrobot_base_vg')
         # Initiate output message as an empty list
         self.message = []
 
         # Note: after the instantiation,
         # a ".client" member must be added to the object
 
-    def verifyCommand(self, command):
+    def verify_command(self, command):
         """Verifies that the value of each variable satisfy its limits."""
 
         # Verify that each variable is in its correct range
-        command.rVCA = max(0, command.rVCA)
-        command.rVCA = min(255, command.rVCA)
-        command.rVCB = max(0, command.rVCB)
-        command.rVCB = min(255, command.rVCB)
+        command.r_vca = max(0, command.r_vca)
+        command.r_vca = min(255, command.r_vca)
+        command.r_vcb = max(0, command.r_vcb)
+        command.r_vcb = min(255, command.r_vcb)
 
         # Verify that the selected mode number is available
-        if command.rMCA not in [0x0000, 0x0100, 0x0200]:
-            rospy.signal_shutdown(
-                rospy.get_name() +
-                ": Select the mode number for ch A from" +
-                "0x0000 (release), 0x0100 (grip), or 0x0200 (idle).")
-        if command.rMCB not in [0x0000, 0x0100, 0x0200]:
-            rospy.signal_shutdown(
-                rospy.get_name() +
-                ": Select the mode number for ch B from" +
-                "0x0000 (release), 0x0100 (grip), or 0x0200 (idle).")
+        if command.r_mca not in [0x0000, 0x0100, 0x0200]:
+            self.get_logger().error(
+                "Select the mode number for ch A from "
+                "0x0000 (release), 0x0100 (grip), or 0x0200 (idle)."
+            )
+            rclpy.shutdown()
+        if command.r_mcb not in [0x0000, 0x0100, 0x0200]:
+            self.get_logger().error(
+                "Select the mode number for ch B from "
+                "0x0000 (release), 0x0100 (grip), or 0x0200 (idle)."
+            )
+            rclpy.shutdown()
 
         # Return the modified command
         return command
 
-    def refreshCommand(self, command):
+    def refresh_command(self, command):
         """Updates the command which will be sent
-           during the next sendCommand() call.
+           during the next send_command() call.
         """
 
         # Limit the value of each variable
-        command = self.verifyCommand(command)
+        command = self.verify_command(command)
 
         # Initiate command as an empty list
         self.message = []
 
         # Build the command with each output variable
-        self.message.append(command.rMCA)
-        self.message.append(command.rVCA)
-        self.message.append(command.rMCB)
-        self.message.append(command.rVCB)
+        self.message.append(command.r_mca)
+        self.message.append(command.r_vca)
+        self.message.append(command.r_mcb)
+        self.message.append(command.r_vcb)
 
-    def sendCommand(self):
+    def send_command(self):
         """Sends the command to the Gripper."""
 
-        self.client.sendCommand(self.message)
+        self.client.send_command(self.message)
 
-    def getStatus(self):
+    def get_status(self):
         """Requests the status from the gripper and
            return it in the OnRobotVGInput msg type.
         """
 
         # Acquire status from the Gripper
-        status = self.client.getStatus()
+        status = self.client.get_status()
 
         # Message to output
         message = OnRobotVGInput()
 
         # Assign the values to their respective variables
-        message.gVCA = status[0]
-        message.gVCB = status[1]
+        message.g_vca = status[0]
+        message.g_vcb = status[1]
 
         return message
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    onrobot_base_vg = OnRobotBaseVG()
+    
+    try:
+        rclpy.spin(onrobot_base_vg)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        onrobot_base_vg.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
--- onrobot_vg_modbus_tcp/onrobot_vg_modbus_tcp/comModbusTcp.py
+++ onrobot_vg_modbus_tcp/onrobot_vg_modbus_tcp/comModbusTcp.py
@@ -5,30 +5,28 @@ OnRobot Grippers using the Modbus/TCP protocol.
 """
 
 import sys
-import rospy
 import threading
 from pymodbus.client.sync import ModbusTcpClient
+import rclpy
+from rclpy.node import Node
 
 
-class communication:
+class Communication(Node):
 
     def __init__(self, dummy=False):
+        super().__init__('communication_node')
         self.client = None
         self.dummy = dummy
         self.lock = threading.Lock()
 
-    def connectToDevice(self, ip, port, changer_addr=65):
+    def connect_to_device(self, ip, port, changer_addr=65):
         """Connects to the client.
            The method takes the IP address and port number
            (as a string, e.g. '192.168.1.1' and '502') as arguments.
         """
         if self.dummy:
-            rospy.loginfo(
-                rospy.get_name() +
-                ": " +
-                sys._getframe().f_code.co_name)
+            self.get_logger().info(f"{self.get_name()}: {sys._getframe().f_code.co_name}")
             return
-
         self.client = ModbusTcpClient(
             ip,
             port=port,
@@ -40,47 +38,36 @@ class communication:
         self.changer_addr = changer_addr
         self.client.connect()
 
-    def disconnectFromDevice(self):
+    def disconnect_from_device(self):
         """Closes connection."""
         if self.dummy:
-            rospy.loginfo(
-                rospy.get_name() +
-                ": " +
-                sys._getframe().f_code.co_name)
+            self.get_logger().info(f"{self.get_name()}: {sys._getframe().f_code.co_name}")
             return
-
         self.client.close()
 
-    def sendCommand(self, message):
+    def send_command(self, message):
         """Sends a command to the Gripper.
            The method takes a list of uint8 as an argument.
         """
         if self.dummy:
-            rospy.loginfo(
-                rospy.get_name() +
-                ": " +
-                sys._getframe().f_code.co_name)
+            self.get_logger().info(f"{self.get_name()}: {sys._getframe().f_code.co_name}")
             return
-
         # Send a command to the device (address 0 ~ 1)
-        if message != []:
-            command = [message[0]+message[1],
-                       message[2]+message[3]]
+        if message:
+            command = [message[0] + message[1],
+                       message[2] + message[3]]
             with self.lock:
                 self.client.write_registers(
                     address=0, values=command, unit=self.changer_addr)
 
-    def getStatus(self):
+    def get_status(self):
         """Sends a request to read, wait for the response
            and returns the Gripper status.
            The method gets by specifying register address as an argument.
         """
         response = [0] * 2
         if self.dummy:
-            rospy.loginfo(
-                rospy.get_name() +
-                ": " +
-                sys._getframe().f_code.co_name)
+            self.get_logger().info(f"{self.get_name()}: {sys._getframe().f_code.co_name}")
             return response
 
         # Get status from the device (address 258 ~ 259)
@@ -90,3 +77,20 @@ class communication:
 
         # Output the result
         return response
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    communication_node = Communication(dummy=False)
+
+    try:
+        rclpy.spin(communication_node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        communication_node.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
